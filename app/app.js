bb7b4c73fdadc65fff5bb26f0f0bab1f52f3ae66
Rachel <BlantonAlmeida@gmail.com>
2016-01-07 16:11:22 -0500

    debugging and reorganizing before refactor

--- app/app.js
+++ app/app.js
@@ -10,31 +10,31 @@
         controller: 'MainController as mainCtrl'
       })
       .when('/admin/editproduct/:itemIndex', {
-        templateUrl:'views/admin/editItem.html',
+        templateUrl:'app/views/admin/editItem.html',
         controller: 'AdminController as adminCtrl'
       })
       .when('/admin/listView', {
-        templateUrl: 'views/admin/listView.html',
+        templateUrl: 'app/views/admin/listView.html',
         controller: 'AdminController as adminCtrl'
       })
       .when('/admin/addproduct', {
-      templateUrl: 'views/admin/addNewItem.html',
+      templateUrl: 'app/views/admin/addNewItem.html',
       controller: 'AdminController as adminCtrl'
       })
       .when('/user/listView', {
-        templateUrl: 'views/user/listView.html',
+        templateUrl: 'app/views/user/listView.html',
         controller: 'MainController as mainCtrl'
       })
       .when('/user/cart', {
-        templateUrl: 'views/user/shoppingCart.html',
+        templateUrl: 'app/views/user/shoppingCart.html',
         controller: 'MainController as mainCtrl'
       })
       .when('/user/detail/:itemIndex', {
-        templateUrl: 'views/user/detailView.html',
+        templateUrl: 'app/views/user/detailView.html',
         controller: 'MainController as mainCtrl'
       })
       .when('/not-found', {
-        templateUrl: 'views/not-found.html'
+        templateUrl: 'app/views/not-found.html'
       })
       .otherwise({
         redirectTo:'/not-found'
