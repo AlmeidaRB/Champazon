2c60598a2cda5c4b97c9bea3e10350d989b27f2b
Rachel <BlantonAlmeida@gmail.com>
2016-01-07 16:38:12 -0500

    refactoringing for  modular components

--- app/app.js
+++ app/app.js
@@ -6,35 +6,35 @@
   .config(function ($routeProvider) {   //configuring routing
     $routeProvider
       .when('/', {
-        templateUrl: 'app/views/main.html',
+        templateUrl: 'app/components/home/main.html',
         controller: 'MainController as mainCtrl'
       })
       .when('/admin/editproduct/:itemIndex', {
-        templateUrl:'app/views/admin/editItem.html',
+        templateUrl:'app/components/admin/editItem.html',
         controller: 'AdminController as adminCtrl'
       })
       .when('/admin/listView', {
-        templateUrl: 'app/views/admin/listView.html',
+        templateUrl: 'app/components/admin/listView.html',
         controller: 'AdminController as adminCtrl'
       })
       .when('/admin/addproduct', {
-      templateUrl: 'app/views/admin/addNewItem.html',
+      templateUrl: 'app/components/admin/addNewItem.html',
       controller: 'AdminController as adminCtrl'
       })
       .when('/user/listView', {
-        templateUrl: 'app/views/user/listView.html',
+        templateUrl: 'app/components/user/listView.html',
         controller: 'MainController as mainCtrl'
       })
       .when('/user/cart', {
-        templateUrl: 'app/views/user/shoppingCart.html',
+        templateUrl: 'app/components/user/shoppingCart.html',
         controller: 'MainController as mainCtrl'
       })
       .when('/user/detail/:itemIndex', {
-        templateUrl: 'app/views/user/detailView.html',
+        templateUrl: 'app/components/user/detailView.html',
         controller: 'MainController as mainCtrl'
       })
       .when('/not-found', {
-        templateUrl: 'app/views/not-found.html'
+        templateUrl: 'app/components/home/not-found.html'
       })
       .otherwise({
         redirectTo:'/not-found'
