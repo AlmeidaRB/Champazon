20c9cb9fd3e4ceb059d1adc35c72f89c812a720b
Rachel <BlantonAlmeida@gmail.com>
2016-01-09 14:16:12 -0500

    refactoring to make modular comps

--- app/components/product/prod1/controller.js
+++ app/components/product/prod1/controller.js
@@ -3,37 +3,83 @@
 
 // user controller
   angular.module('champazon')
-    .controller('MainController', function (UserService, $rootScope, $scope, $routeParams, $location) {
+    .controller('MainController', function (StoreService, $rootScope, $scope, $routeParams, $location) {
       var mainCtrl = this;
 
-      UserService.getProducts().success(function(data) {
-        mainCtrl.Products = data;
+      StoreService.getItems().success(function(data) {
+        mainCtrl.items = data;
       });
 
-      UserService.getProduct($routeParams.productIndex).success(function(data) {
-        mainCtrl.singleProduct = data;
+      StoreService.getItem($routeParams.itemIndex).success(function(data) {
+        mainCtrl.singleItem = data;
       });
 
-      mainCtrl.currentIndex = $routeParams.productIndex;
+      mainCtrl.currentIndex = $routeParams.itemIndex;
 
   // shopping cart
-        mainCtrl.cart = UserService.getCartProducts();
-        mainCtrl.total = UserService.total();
+        mainCtrl.cart = StoreService.getCartItems();
+        mainCtrl.total = StoreService.total();
 
-        mainCtrl.addToCart = function(product) {
-          UserService.addToCart(product);
+        mainCtrl.addToCart = function(item) {
+          StoreService.addToCart(item);
           $location.path('/user/cart');
         };
 
-        mainCtrl.deleteFromCart = function(product) {
-          UserService.deleteFromCart(product);
+        mainCtrl.deleteFromCart = function(item) {
+          StoreService.deleteFromCart(item);
         };
 
-        mainCtrl.addReview = function (product, review) {
-          UserService.addReview(product, review);
+        mainCtrl.addReview = function (item, review) {
+          StoreService.addReview(item, review);
           $scope.review = {};
         };
 
       });
 
+})();
+
+//admin controller
+
+(function() {
+  "use strict";
+  angular.module('champazon')
+    .controller('AdminController', function (StoreService, $rootScope, $scope, $routeParams, $location) {
+
+      var adminCtrl = this;  //alias for AdminController
+
+      StoreService.getItems().success(function(data) {
+        adminCtrl.items = data;
+      });
+
+      StoreService.getItem($routeParams.itemIndex).success(function(data) {
+        adminCtrl.singleItem = data;
+      });
+
+      adminCtrl.currentIndex = $routeParams.itemIndex;
+
+      //login feature//
+      adminCtrl.login = function (username) {
+        if(username === 'rach') {
+          $location.path('/admin/listView');
+        } else {
+          alert("Incorrect admin username, please try again.");
+        }
+      };
+
+      adminCtrl.addProduct = function (newItem) {
+        StoreService.addItem(newItem);
+        $location.path('/admin/listView');
+
+      };
+
+      adminCtrl.deleteProduct = function (id) {
+        StoreService.deleteItem(id);
+      };
+
+      adminCtrl.editProduct = function (item) {
+        StoreService.editItem(item, $routeParams.itemIndex);
+        $location.path('/admin/listView');
+      };
+
+    });
 })();
