9396ae71a4f49a9938d00c4aaca966cee1d73d5c
Rachel <BlantonAlmeida@gmail.com>
2016-01-09 14:05:55 -0500

    refactoring to make modular comps

--- app/components/product/prod1/service.js
+++ app/components/product/prod1/service.js
@@ -2,63 +2,81 @@
 "use strict";
 
 angular.module('champazon')
-  .factory('UserService', function ($http, $rootScope) {
+  .factory('StoreService', function ($http, $rootScope) {
 
     var url = 'https://tiny-tiny.herokuapp.com/collections/champazon';
 
-// Functions for the User
+// Functions for the Admin
 
     var getProducts = function () {
       return $http.get(url);
     };
-    var getSingleProduct = function (id) {
+    var getSingleItem = function (id) {  // was (index)
       return $http.get(url + '/' +id);
     };
-    var addReview = function (product, review) {
+    var addProduct = function (item) {
+      item.reviews = [];
+      $http.post(url, item);
+      $rootScope.$broadcast('item:created')
+    };
+    var deleteProduct = function (id) {   //was (item)
+      $http.delete(url + '/' + id);
+      $rootScope.$broadcast('item:deleted');
+    };
+    var editProduct = function (item, id) {  //was (item, index)
+      $http.put(url + '/' + id, item);
+      $rootScope.$broadcast('item:updated');
+    };
+
+// Functions for the User
+
+    var addReview = function (item, review) {
       review = {
         stars: 0,
         body: '',
         author: ''
       }
-      product.reviews.push(review);
-      $http.put(url + '/' + product._id, product);
+      item.reviews.push(review);
+      $http.put(url + '/' + item._id, item);
     };
 
 //Functions for the Cart
 
     var cart = [];
 
-    var addToCart = function (product) {
-      cart.push(product);
+    var addToCart = function (item) {
+      cart.push(item);
     };
 
     var getCartProducts = function () {
       return cart;
     };
 
-    var deleteCartProducts = function (product) {
-      var idx = cart.indexOf(product);
+    var deleteCartProducts = function (item) {
+      var idx = cart.indexOf(item);
       cart.splice(idx,1);
     };
 
     var total = function() {
       var total = 0;
-      angular.forEach(cart, function(product) {
-      total += product.quant * product.price;
+      angular.forEach(cart, function(item) {
+      total += item.quant * item.price;
       })
       return total;
     };
 
     return {
-      
-      getProducts: getProducts,
-      getProduct: getSingleProduct,
+      getItems: getProducts,
+      getItem: getSingleItem,
+      addItem: addProduct,
+      deleteItem: deleteProduct,
+      editItem: editProduct,
       addReview: addReview,
+
       addToCart: addToCart,
-      getCartProducts: getCartProducts,
+      getCartItems: getCartProducts,
       deleteFromCart: deleteCartProducts,
       total: total
-
     };
 
   });
